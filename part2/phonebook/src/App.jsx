25ff71f2bbe4582b7b2bb376bd163e71633de50a
REYES ALVAREZ PATRICIO <reyesalvarez.patricio@gmail.com>
2025-03-12 15:53:54 -0600

    Complete 2.6: phonebook

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const AddPerson = (event) => {
+    event.preventDefault();
+    const personObject = {
+      name: newName,
+    };
+    setPersons(persons.concat(personObject));
+    setNewName("");
+  };
+
+  const handleInputChange = (event) => {
+    setNewName(event.target.value);
+  };
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={AddPerson}>
+        <div>
+          Name: <input value={newName} onChange={handleInputChange} />
+        </div>
+        <div>
+          <button type="submit">Add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((x) => (
+        <li key={x.name}>{x.name}</li>
+      ))}
+    </div>
+  );
+};
+
+export default App;
