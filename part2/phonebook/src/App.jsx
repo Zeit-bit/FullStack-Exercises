967888ef0286d13cd6cc360886cb1e0e9b3c34ab
REYES ALVAREZ PATRICIO <reyesalvarez.patricio@gmail.com>
2025-03-13 10:11:00 -0600

    Complete 2.11: phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Filter = ({ filter, setFilter, HandleInputChange }) => {
   return (
@@ -56,13 +57,16 @@ const Contacts = ({ personsToShow }) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then((response) => setPersons(response.data));
+  }, []);
 
   const AddPerson = (event) => {
     event.preventDefault();
