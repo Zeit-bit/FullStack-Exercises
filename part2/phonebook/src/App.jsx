66903b538b2550fb518cafba8f048dd6027a644d
REYES ALVAREZ PATRICIO <reyesalvarez.patricio@gmail.com>
2025-03-12 20:10:59 -0600

    Complete 2.10: phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,8 +1,64 @@
 import { useState } from "react";
 
+const Filter = ({ filter, setFilter, HandleInputChange }) => {
+  return (
+    <div>
+      Filter shown with:
+      <input
+        value={filter}
+        onChange={(event) => HandleInputChange(event, setFilter)}
+      />
+    </div>
+  );
+};
+
+const ContactForm = ({
+  newName,
+  setNewName,
+  newNumber,
+  setNewNumber,
+  AddPerson,
+  HandleInputChange,
+}) => {
+  return (
+    <form onSubmit={AddPerson}>
+      <div>
+        Name:{" "}
+        <input
+          value={newName}
+          onChange={(event) => HandleInputChange(event, setNewName)}
+        />
+      </div>
+      <div>
+        Number:{" "}
+        <input
+          value={newNumber}
+          onChange={(event) => HandleInputChange(event, setNewNumber)}
+        />
+      </div>
+      <div>
+        <button type="submit">Add</button>
+      </div>
+    </form>
+  );
+};
+
+const Contacts = ({ personsToShow }) => {
+  return (
+    <div>
+      {personsToShow.map((p) => (
+        <li key={p.name}>
+          {p.name} ({p.number})
+        </li>
+      ))}
+    </div>
+  );
+};
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: "Arto Hellas", number: "040-1234567" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
@@ -27,15 +83,13 @@ const App = () => {
     setNewNumber("");
   };
 
-  const HandleInputChange = (event, setValue) => {
-    setValue(event.target.value);
-  };
+  const HandleInputChange = (event, setValue) => setValue(event.target.value);
 
   const personsToShow =
     filter.length > 0
       ? persons.filter((p) => {
-          let personNameTrimmed = p.name.slice(0, filter.length);
-          if (personNameTrimmed.toLowerCase() === filter.toLowerCase()) {
+          let personNameSliced = p.name.slice(0, filter.length);
+          if (personNameSliced.toLowerCase() === filter.toLowerCase()) {
             return true;
           }
         })
@@ -44,39 +98,25 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-        Filter shown with:
-        <input
-          value={filter}
-          onChange={(event) => HandleInputChange(event, setFilter)}
-        />
-      </div>
+
+      <Filter
+        filter={filter}
+        setFilter={setFilter}
+        HandleInputChange={HandleInputChange}
+      />
+
       <h2>Add a new contact</h2>
-      <form onSubmit={AddPerson}>
-        <div>
-          Name:{" "}
-          <input
-            value={newName}
-            onChange={(event) => HandleInputChange(event, setNewName)}
-          />
-        </div>
-        <div>
-          Number:{" "}
-          <input
-            value={newNumber}
-            onChange={(event) => HandleInputChange(event, setNewNumber)}
-          />
-        </div>
-        <div>
-          <button type="submit">Add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      {personsToShow.map((p) => (
-        <li key={p.name}>
-          {p.name} ({p.number})
-        </li>
-      ))}
+      <ContactForm
+        newName={newName}
+        setNewName={setNewName}
+        newNumber={newNumber}
+        setNewNumber={setNewNumber}
+        AddPerson={AddPerson}
+        HandleInputChange={HandleInputChange}
+      />
+
+      <h2>Contacts</h2>
+      <Contacts personsToShow={personsToShow} />
     </div>
   );
 };
