867feea0a4a739f6a6ebe3884142ebc3f06656bb
REYES ALVAREZ PATRICIO <reyesalvarez.patricio@gmail.com>
2025-03-25 20:36:05 -0600

    Complete 4.8: bloglist-tests

---
+++ Part4/bloglist/tests/blogRequests.test.js
@@ -0,0 +1,74 @@
+const supertest = require('supertest')
+const { test, describe, after, beforeEach } = require('node:test')
+const assert = require('node:assert')
+const app = require('../app')
+const { default: mongoose } = require('mongoose')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+const initialBlogs = [
+  {
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7
+  },
+  {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5
+  },
+  {
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    likes: 12
+  },
+  {
+    title: 'First class tests',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+    likes: 10
+  },
+  {
+    title: 'TDD harms architecture',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+    likes: 0
+  },
+  {
+    title: 'Type wars',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+    likes: 2
+  }
+]
+
+describe('When there are blogs in the database', () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    for (const blog in initialBlogs) {
+      const newBlog = new Blog(initialBlogs[blog])
+      await newBlog.save()
+    }
+  })
+
+  describe('When using GET /api/blogs', () => {
+    test('the amount of blogs returned is correct', async () => {
+      const response = await api.get('/api/blogs')
+      assert.strictEqual(response.body.length, initialBlogs.length)
+    })
+
+    test('the returned blogs are in json format', async () => {
+      await api.get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+    })
+  })
+
+  after(async () => {
+    await mongoose.connection.close()
+  })
+})
